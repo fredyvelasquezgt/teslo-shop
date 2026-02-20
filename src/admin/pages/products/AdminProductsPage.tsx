22ca8501d58a4e96b9974f1a3112ab49db037d05
Fredy <macacoloco696@gmail.com>
2026-02-20 16:10:02 -0600

    commit

--- src/admin/pages/products/AdminProductsPage.tsx
+++ src/admin/pages/products/AdminProductsPage.tsx
@@ -1,9 +1,67 @@
+import { AdminTitle } from '@/admin/components/AdminTitle'
+import { CustomPagination } from '@/components/custom/CustomPagination';
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table'
+import { PlusIcon } from 'lucide-react';
 import React from 'react'
+import { Link } from 'react-router';
+import { Button } from '@/components/ui/button';
 
 export const AdminProductsPage = () => {
     return (
-        <div>
-            AdminProductsPage
-        </div>
+        <>
+            <div className="flex justify-between items-center">
+                <AdminTitle title="Productos" subTitle="Aqui puedes ver y administrar los productos" />
+                <div className="flex justify-end mb-10 gap-4">
+                    <Link to="/admin/products/new" >
+                        <Button>
+                            <PlusIcon />
+                            Nuevo producto
+                        </Button>
+                    </Link>
+                </div>
+
+            </div>
+
+            <Table className="bg-white p-10 shadow-xs border border-gray-200 mb-10">
+                <TableHeader>
+                    <TableRow>
+                        <TableHead className="w-[100px]">ID</TableHead>
+                        <TableHead>Imagen</TableHead>
+                        <TableHead>Nombre</TableHead>
+                        <TableHead>Precio</TableHead>
+                        <TableHead>Categoria</TableHead>
+                        <TableHead>Inventario</TableHead>
+                        <TableHead>Tallas</TableHead>
+                        <TableHead className="text-right">Acciones</TableHead>
+                    </TableRow>
+                </TableHeader>
+                <TableBody>
+                    <TableRow>
+                        <TableCell className="font-medium">1</TableCell>
+                        <TableCell>
+                            <img
+                                src='https://placehold.co/250x250'
+                                alt='product'
+                                className='w-20 h-20 object-cover rounded-md'
+                            />
+                        </TableCell>
+                        <TableCell>Producto 1</TableCell>
+                        <TableCell>$250.00</TableCell>
+                        <TableCell>Categoria</TableCell>
+                        <TableCell>100 stock</TableCell>
+                        <TableCell>XS,S,L</TableCell>
+
+
+                        <TableCell className="text-right">
+                            <Link to={`/admin/products/t-shirt-teslo`}>
+                                Editar
+                            </Link>
+                        </TableCell>
+
+                    </TableRow>
+                </TableBody>
+            </Table>
+            <CustomPagination totalPages={10} />
+        </>
     )
 }
--- src/shop/pages/gender/GenderPage.tsx
+++ src/shop/pages/gender/GenderPage.tsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import { CustomJumbotron } from '../../components/CustomJumbotron';
+import { ProductsGrid } from '../../components/ProductsGrid';
+import { CustomPagination } from '../../../components/custom/CustomPagination';
+import { products } from '../../../mocks/products.mock';
+import { useParams } from 'react-router';
 
 export const GenderPage = () => {
+
+    const { gender } = useParams()
+    const genderLabel = gender === 'men'
+        ? 'Hombre'
+        : gender === 'women'
+            ? 'Mujeres'
+            : 'Niños'
+
     return (
-        <div>
-            GenderPage
-        </div>
+        <>
+            <CustomJumbotron title={`Productos para ${genderLabel}`} />
+
+            <ProductsGrid products={products} />
+            <CustomPagination totalPages={7} />
+        </>
     )
 }
