7621c6a7a2007f597c63de7a34ac9790c3253e95
Fredy <macacoloco696@gmail.com>
2026-03-10 23:19:33 -0600

    admin panel

--- src/admin/pages/products/AdminProductsPage.tsx
+++ src/admin/pages/products/AdminProductsPage.tsx
@@ -1,12 +1,23 @@
 import { AdminTitle } from '@/admin/components/AdminTitle'
 import { CustomPagination } from '@/components/custom/CustomPagination';
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table'
-import { PlusIcon } from 'lucide-react';
+import { PencilIcon, PlusIcon } from 'lucide-react';
 import React from 'react'
 import { Link } from 'react-router';
 import { Button } from '@/components/ui/button';
+import { useProducts } from '../../../shop/hooks/useProducts';
+import { join } from 'path';
+import { CustomFullScreenLoading } from '@/components/custom/CustomFullScreenLoading';
+import { currencyFormatter } from '../../../lib/currency-formatter';
 
 export const AdminProductsPage = () => {
+
+    const { data, isLoading } = useProducts()
+
+    if (isLoading) {
+        return <CustomFullScreenLoading />
+    }
+
     return (
         <>
             <div className="flex justify-between items-center">
@@ -25,7 +36,6 @@ export const AdminProductsPage = () => {
             <Table className="bg-white p-10 shadow-xs border border-gray-200 mb-10">
                 <TableHeader>
                     <TableRow>
-                        <TableHead className="w-[100px]">ID</TableHead>
                         <TableHead>Imagen</TableHead>
                         <TableHead>Nombre</TableHead>
                         <TableHead>Precio</TableHead>
@@ -36,32 +46,41 @@ export const AdminProductsPage = () => {
                     </TableRow>
                 </TableHeader>
                 <TableBody>
-                    <TableRow>
-                        <TableCell className="font-medium">1</TableCell>
-                        <TableCell>
-                            <img
-                                src='https://placehold.co/250x250'
-                                alt='product'
-                                className='w-20 h-20 object-cover rounded-md'
-                            />
-                        </TableCell>
-                        <TableCell>Producto 1</TableCell>
-                        <TableCell>$250.00</TableCell>
-                        <TableCell>Categoria</TableCell>
-                        <TableCell>100 stock</TableCell>
-                        <TableCell>XS,S,L</TableCell>
+                    {
+                        data!.products.map((product) => (
+                            <TableRow>
+                                <TableCell>
+                                    <img
+                                        src={product.images[0]}
+                                        alt='product'
+                                        className='w-20 h-20 object-cover rounded-md'
+                                    />
+                                </TableCell>
+                                <TableCell>
+                                    <Link to={`/admin/products/${product.id}`} className="hover:text-blue-500 underline" >
+                                        {product.title}
+                                    </Link>
+                                </TableCell>
+                                <TableCell>{currencyFormatter(product.price)}</TableCell>
+                                <TableCell>{product.gender}</TableCell>
+                                <TableCell>{product.stock}</TableCell>
+                                <TableCell>{product.sizes.join(',')}</TableCell>
 
 
-                        <TableCell className="text-right">
-                            <Link to={`/admin/products/t-shirt-teslo`}>
-                                Editar
-                            </Link>
-                        </TableCell>
+                                <TableCell className="text-right">
+                                    <Link to={`/admin/products/${product.id}`} >
 
-                    </TableRow>
+                                        <PencilIcon className="w-4 h-4 text-blue-500" />
+                                    </Link>
+                                </TableCell>
+
+                            </TableRow>
+                        ))
+                    }
+
                 </TableBody>
             </Table>
-            <CustomPagination totalPages={10} />
+            <CustomPagination totalPages={data?.pages || 0} />
         </>
     )
 }
